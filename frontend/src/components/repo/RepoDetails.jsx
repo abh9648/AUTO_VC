47b344641612dd2b35375a860e5563ff96ae6dd5
Abhishek Kushwaha <kushwahaabhishek5126@email.com>
2026-06-18 00:14:11 +0530

    Fix CSS filename case

--- frontend/src/components/repo/RepoDetails.jsx
+++ frontend/src/components/repo/RepoDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../Navbar";
-import "./repoDetails.css";
+import "./RepoDetails.css";
 
 const RepoDetails = () => {
   const { id } = useParams();
